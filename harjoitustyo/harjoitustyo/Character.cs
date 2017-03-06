8b2ccaaf0e783960f4505982f1541556aace8535
K9090 <K9090@student.jamk.fi>
2017-03-06 12:59:27 +0200

    Classes ok'ish

---
+++ harjoitustyo/harjoitustyo/Character.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harjoitustyo
+{
+    class Character
+    {
+        public string Name { get; set; }
+        public int Hitpoints { get; set; }
+        public bool HasWeapon { get; set; }
+        public bool UsesAmmo { get; set; }
+
+        public void Move()
+        {
+
+        }
+
+        public void Fight()
+        {
+
+        }
+
+        public void Die()
+        {
+
+        }
+
+        public void AlterHitPoints()
+        {
+
+        }
+        
+    }
+
+    class Enemy : Character
+    {
+        public int ScoreValue { get; set; }
+        public int Accuracy { get; set; }
+
+        public void AttackPlayer()
+        {
+
+        }
+    }
+
+    class Player : Character
+    {
+        List<Weapon> WeaponList { get; set; }
+        public int Ammo { get; set; }
+        public int Score { get; set; }
+
+        public void GainScore()
+        {
+
+        }
+
+        public void ExitWhenDead()
+        {
+
+        }
+    }
+}
